4d036b21439d1609f09e162410f5c82632ce648c
cjun714 <cjun714@gmail.com>
2021-04-19 10:29:35 +0800

    feat: Load() load bytes in go and parse it to stb

--- stb/loader.go
+++ stb/loader.go
@@ -6,22 +6,19 @@ package stb
 import "C"
 import (
 	"errors"
+	"fmt"
+	"io/ioutil"
 	"unsafe"
 )
 
 // Load is to load image file from a specified path
 func Load(path string) (*uint8, int, int, int, error) {
-	var x, y, comp int32
-	dataPtr := (*uint8)(
-		C.stbi_load(C.CString(path),
-			(*C.int)(unsafe.Pointer(&x)),
-			(*C.int)(unsafe.Pointer(&y)),
-			(*C.int)(unsafe.Pointer(&comp)),
-			0))
-	if dataPtr == nil {
-		return nil, 0, 0, 0, errors.New("Load image failed: " + path)
+	byts, e := ioutil.ReadFile(path)
+	if e != nil {
+		return nil, 0, 0, 0, fmt.Errorf("read image failed %s, %w", path, e)
 	}
-	return dataPtr, int(x), int(y), int(comp), nil
+
+	return LoadBytes(byts)
 }
 
 // LoadBytes is to load image from []byte
@@ -35,7 +32,7 @@ func LoadBytes(data []byte) (*uint8, int, int, int, error) {
 		(*C.int)(unsafe.Pointer(&comp)),
 		0))
 	if dataPtr == nil {
-		return nil, 0, 0, 0, errors.New("Load image failed")
+		return nil, 0, 0, 0, errors.New("decode image failed")
 	}
 	return dataPtr, int(x), int(y), int(comp), nil
 }
